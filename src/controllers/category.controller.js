d2f86dbea5b1b178084d0e5007c579d7175ea26a
Renatolbn <renatolbn@gmail.com>
2026-05-03 14:38:03 -0300

    feat: category controller implementado

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,6 +1,47 @@
-const getAll = (req, res) => res.json({ message: 'getAll' })
-const create = (req, res) => res.json({ message: 'create' })
-const update = (req, res) => res.json({ message: 'update' })
-const remove = (req, res) => res.json({ message: 'remove' })
+import Category from "../models/category.js";
 
-export default { getAll, create, update, remove }
+const getAll = async (req, res) => {
+  try {
+    const categories = await Category.find().sort({ name: 1 });
+    res.json(categories);
+  } catch (err) {
+    res.status(500).json({ error: "Erro ao buscar categorias" });
+  }
+};
+
+const create = async (req, res) => {
+  try {
+    const categories = await Category.create(req.body);
+    res.status(201).json(categories);
+  } catch (err) {
+    res.status(500).json({ erro: "Erro ao criar categorias" });
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const categories = await Category.findByIdAndUpdate(
+      req.params.id, // qual documento atualizar
+      req.body, // com quais dados
+      { new: true, runValidators: true }, // opções
+    );
+    if (!categories)
+      return res.status(404).json({ error: "Categoria não encontrada" });
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json({ erro: "Erro ao atualizar categorias" });
+  }
+};
+
+const remove = async (req, res) => {
+  try {
+    const categories = await Category.findByIdAndDelete(req.params.id);
+    if (!categories)
+      return res.status(404).json({ error: "Categoria não encontrada" });
+    res.status(200).json({ message: "Categoria removida com sucesso"});
+  } catch (err) {
+    res.status(500).json({ erro: "Erro ao deletar categorias" });
+  }
+};
+
+export default { getAll, create, update, remove };
