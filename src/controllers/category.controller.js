ab9d6a917207c55cf534b19d7525ac6476a727e7
Renatolbn <renatolbn@gmail.com>
2026-05-04 15:28:58 -0300

    chore: ajuste de espaçamento

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -14,7 +14,7 @@ const create = async (req, res) => {
     const categories = await Category.create(req.body);
     res.status(201).json(categories);
   } catch (err) {
-    res.status(500).json({ erro: "Erro ao criar categorias" });
+    res.status(500).json({ error: "Erro ao criar categorias" });
   }
 };
 
@@ -25,22 +25,24 @@ const update = async (req, res) => {
       req.body, // com quais dados
       { new: true, runValidators: true }, // opções
     );
+
     if (!categories)
       return res.status(404).json({ error: "Categoria não encontrada" });
     res.status(200).json(categories);
   } catch (err) {
-    res.status(500).json({ erro: "Erro ao atualizar categorias" });
+    res.status(500).json({ error: "Erro ao atualizar categorias" });
   }
 };
 
 const remove = async (req, res) => {
   try {
     const categories = await Category.findByIdAndDelete(req.params.id);
+    
     if (!categories)
       return res.status(404).json({ error: "Categoria não encontrada" });
     res.status(200).json({ message: "Categoria removida com sucesso"});
   } catch (err) {
-    res.status(500).json({ erro: "Erro ao deletar categorias" });
+    res.status(500).json({ error: "Erro ao deletar categorias" });
   }
 };
 
