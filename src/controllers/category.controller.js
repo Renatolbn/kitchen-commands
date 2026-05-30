7f9c92b1cc04d0850ac548edf4e9170b605c72c6
Renatolbn <renatolbn@gmail.com>
2026-05-30 16:41:05 -0300

    chore: ajustes no category controller

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -2,47 +2,65 @@ import Category from "../models/category.js";
 
 const getAll = async (req, res) => {
   try {
-    const categories = await Category.find().sort({ name: 1 });
-    res.json(categories);
+    const category = await Category.find().sort({ name: 1 });
+    res.json(category);
   } catch (err) {
-    res.status(500).json({ error: "Erro ao buscar categorias" });
+    console.error(err.message);
+
+    if (err.name === "CastError") {
+      return res.status(400).json({ error: "ID inválido" });
+    }
+
+    res.status(500).json({ error: "Erro ao buscar categoria" });
   }
 };
 
 const create = async (req, res) => {
   try {
-    const categories = await Category.create(req.body);
-    res.status(201).json(categories);
+    const category = await Category.create(req.body);
+    res.status(201).json(category);
   } catch (err) {
-    res.status(500).json({ error: "Erro ao criar categorias" });
+    console.error(err.message);
+    res.status(500).json({ error: "Erro ao criar categoria" });
   }
 };
 
 const update = async (req, res) => {
   try {
-    const categories = await Category.findByIdAndUpdate(
-      req.params.id, // qual documento atualizar
-      req.body, // com quais dados
-      { new: true, runValidators: true }, // opções
-    );
+    const category = await Category.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-    if (!categories)
+    if (!category)
       return res.status(404).json({ error: "Categoria não encontrada" });
-    res.status(200).json(categories);
+    res.status(200).json(category);
   } catch (err) {
-    res.status(500).json({ error: "Erro ao atualizar categorias" });
+    console.error(err.message);
+
+    if (err.name === "CastError") {
+      return res.status(400).json({ error: "ID inválido" });
+    }
+
+    res.status(500).json({ error: "Erro ao atualizar categoria" });
   }
 };
 
 const remove = async (req, res) => {
   try {
-    const categories = await Category.findByIdAndDelete(req.params.id);
-    
-    if (!categories)
+    const category = await Category.findByIdAndDelete(req.params.id);
+
+    if (!category)
       return res.status(404).json({ error: "Categoria não encontrada" });
-    res.status(200).json({ message: "Categoria removida com sucesso"});
+    res.status(200).json({ message: "Categoria removida com sucesso" });
   } catch (err) {
-    res.status(500).json({ error: "Erro ao deletar categorias" });
+    console.error(err.message);
+
+    if (err.name === "CastError") {
+      return res.status(400).json({ error: "ID inválido" });
+    }
+
+    res.status(500).json({ error: "Erro ao deletar categoria" });
   }
 };
 
