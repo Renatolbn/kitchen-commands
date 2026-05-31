184430dce8cf3f373e37a86919ba623d058704f9
Renatolbn <renatolbn@gmail.com>
2026-05-31 14:56:07 -0300

    feat: orderItem controller implementado

--- src/controllers/orderItem.controller.js
+++ src/controllers/orderItem.controller.js
@@ -1,5 +1,54 @@
-const getAll = (req, res) => res.json({ message: "getAll" });
-const updateStatus = (req, res) => res.json({ message: "updateStatus" });
-const remove = (req, res) => res.json({ message: "remove" });
+import OrderItem from "../models/orderItem.js";
+
+const getAll = async (req, res) => {
+  try {
+    const orderItem = await OrderItem.find().populate("menuItem", "name price");
+    res.json(orderItem);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.name === "CastError") {
+      return res.status(400).json({
+        error: "ID inválido"});
+    }
+     res.status(500).json({ error: "Erro ao buscar item da comanda" });
+  }
+};
+const updateStatus = async (req, res) => {
+  const { status } = req.body;
+
+  try {
+    const orderItem = await OrderItem.findByIdAndUpdate(
+      req.params.id,
+      { status },
+      { new: true, runValidators: true },
+    );
+    if (!orderItem) return res.status(404).json({ error: "Item do pedido não encontrado" });
+    res.status(200).json(orderItem);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.name === "CastError") {
+      return res.status(400).json({ error: "ID inválido" });
+    }
+    res.status(500).json({ error: "Erro ao atualizar o item do pedido" });
+  }
+};
+
+const remove = async (req, res) => {
+  try {
+    const orderItem = await OrderItem.findByIdAndDelete(req.params.id);
+
+    if (!orderItem) return res.status(404).json({ error: "Item do pedido não encontrado" });
+    res.status(200).json({ message: "Item do pedido removido com sucesso" });
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.name === "CastError") {
+      return res.status(400).json({ error: "ID inválido" });
+    }
+    res.status(500).json({ error: "Erro ao deletar item do pedido" });
+  }
+};
 
 export default { getAll, updateStatus, remove };
