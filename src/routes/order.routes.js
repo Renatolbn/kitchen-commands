e2138e21d37eb6441297935be7da11417a4fbc97
Renatolbn <renatolbn@gmail.com>
2026-07-22 15:22:41 -0300

    feat: middleware de autorização por role implementado

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import orderController from "../controllers/order.controller.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
+import authorize from "../middlewares/authorize.middleware.js";
 
 const router = Router();
-
-router.get("/", orderController.getAll);
-router.post("/", orderController.create);
-router.put("/:id", orderController.update);
-router.patch("/:id/status", orderController.updateStatus);
-router.delete("/:id", orderController.remove);
+// Rotas protegidas
+router.get("/", authMiddleware, orderController.getAll);
+router.post("/", authMiddleware, authorize("waiter"), orderController.create);
+router.put("/:id", authMiddleware,authorize("waiter", "kitchen"), orderController.update);
+router.patch(
+  "/:id/status",
+  authMiddleware,
+  authorize("waiter", "kitchen"),
+  orderController.updateStatus,
+);
+router.delete("/:id", authMiddleware, authorize("admin", "waiter"), orderController.remove);
 
 export default router;
