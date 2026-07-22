21ff60d6f0e2d66f19fdaa4993758a98e229d494
Renatolbn <renatolbn@gmail.com>
2026-07-22 16:39:26 -0300

    feat: middlewares de auth e autorização

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,20 +1,22 @@
 import jwt from "jsonwebtoken";
 
 const authMiddleware = (req, res, next) => {
-    const authHeader = req.headers.authorization;
-    const token = authHeader && authHeader.split(' ')[1];
+  console.log("middleware chamado:", req.method, req.path);
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
 
-    if (!token) { return res.status(401).json({ error: 'Token não fornecido'});
-    }
+  if (!token) {
+    return res.status(401).json({ error: "Token não fornecido" });
+  }
 
-    try {
-        const decoder = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoder;
-        next();
-    } catch (err) {
-        console.error(err.message);
-         res.status(401).json({ error: "Token inválido" });
-    }
-}
+  try {
+    const decoder = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoder;
+    next();
+  } catch (err) {
+    console.error(err.message);
+    res.status(401).json({ error: "Token inválido" });
+  }
+};
 
-export default authMiddleware
+export default authMiddleware;
