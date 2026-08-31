06a877bc1f464f6e79278f6df3e8e7d19e3ff188
Renatolbn <renatolbn@gmail.com>
2026-08-31 20:26:21 -0300

    refactor: configurar CORS na inicialização do Socket.io

--- src/socket.js
+++ src/socket.js
@@ -1,22 +1,29 @@
-import { Server } from 'socket.io'
+import { Server } from "socket.io";
 
-let io
+let io;
 
 const initSocket = (httpServer) => {
-  io = new Server(httpServer)
+  io = new Server(httpServer, {
+    cors: {
+      origin: "http://localhost:5173",
+      credentials: true,
+    },
+  });
 
-  io.on('connection', (socket) => {
-    console.log('Cliente conectado:', socket.id)
+  io.on("connection", (socket) => {
+    console.log("Cliente conectado:", socket.id);
 
-    socket.on('disconnect', () => {
-      console.log('Cliente desconectado:', socket.id)
-    })
-  })
-}
+    socket.on("disconnect", () => {
+      console.log("Cliente desconectado:", socket.id);
+    });
+  });
+};
 
 const getIO = () => {
-  if (!io) throw new Error('Socket.io não inicializado')
-  return io
-}
+  if (!io) {
+    throw new Error("Socket.io não inicializado");
+  }
+  return io;
+};
 
-export { initSocket, getIO }
+export { initSocket, getIO };
